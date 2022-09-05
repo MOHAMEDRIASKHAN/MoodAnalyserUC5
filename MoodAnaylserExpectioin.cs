e9c5ef61349045ddb88816e92d1ce11c5e8f397b
MOHAMEDRIASKHAN <mohamedriaskhan@gmail.com>
2022-09-05 08:07:30 -0700

    MoodAnaylserExpectioin.cs

---
+++ MoodAnaylserExpectioin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserUC5
+{
+    public class MoodAnaylserExpectioin : Exception
+    {
+         
+        public enum ExceptionType
+        {
+            ENTERD_NULL, ENTERED_EMPTY,
+            NO_SUCH_CLASS, NO_SUCH_METHOD
+        }
+
+        public ExceptionType type;
+        public MoodAnaylserExpectioin(ExceptionType type, string message) : base(message)
+        {
+            this.type = type;
+        }
+    
+}
+}
