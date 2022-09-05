9911880554a48cbfc788dc804f1ac0c71b6fd88f
MOHAMEDRIASKHAN <mohamedriaskhan@gmail.com>
2022-09-05 08:03:01 -0700

    MoodAnalyser

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+namespace MoodAnalyserUC5
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
